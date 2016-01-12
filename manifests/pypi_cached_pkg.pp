9b8b0985138d1ca0429b800f7c3bf4c5796190a8
kjhenner <kjhenner@gmail.com>
2016-01-12 14:28:47 -0800

    fix pypi_cached_pkg template syntax issue

--- manifests/pypi_cached_pkg.pp
+++ manifests/pypi_cached_pkg.pp
@@ -2,7 +2,7 @@ define learning::pypi_cached_pkg {
 
   include wget
 
-  $basename = inline_template('<%= File.basename($title) %>')
+  $basename = inline_template("<%= File.basename('${title}') %>")
   wget::fetch { $title:
     destination => "${pypi_pkg_dir}/${basename}",
   }
